a3585183af16b101d0ce3bb0e405306d30eb79f3
lakshmanan5678 <lordslaxman@gmail.co>
2025-06-04 16:01:40 +0530

    services

---
+++ src/Services/Hostel.js
@@ -0,0 +1,60 @@
+
+// import apiInstance from "../interceptors/axios";
+// import { message } from "antd";
+
+// export const createHostel = async (formData) => {
+//   try {
+//     const res = await apiInstance.post("/hostel/create", formData);
+//     message.success("Hostel created successfully!");
+//     return res.data;
+//   } catch (err) {
+//     console.error("Hostel Create Error:", err);
+//     message.error("Hostel creation failed.");
+//     throw err;
+//   }
+// };
+
+
+
+import apiInstance from "../interceptors/axios";
+import { message } from "antd";
+
+export const createHostel = async (data) => {
+  try {
+    const formData = new FormData();
+
+    formData.append("hostelName", data.hostelName);
+    formData.append("numberOfRooms", data.noOfRooms);
+    formData.append("numberOfOccupancies", data.noOfOccupancies);
+    formData.append("contactPerson", data.contactPerson);
+    formData.append("mobileNumber", data.mobileNumber);
+    formData.append("mailId", data.mailId);
+    formData.append("address", data.address);
+    formData.append("city", data.city);
+    formData.append("area", data.area);
+    formData.append("pincode", data.pincode);
+    formData.append("googleMapLink", data.googleMapLink);
+
+
+    data.amenities?.forEach((item) => {
+      formData.append("amenities", item);
+    });
+
+    data.hostelImages.forEach((file) => {
+      formData.append("hostelImages", file);
+    });
+
+    const res = await apiInstance.post("/hostel/create", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    message.success("Hostel created successfully!");
+    return res.data;
+  } catch (err) {
+    console.error("Hostel Create Error:", err.response?.data || err);
+    message.error("Hostel creation failed.");
+    throw err;
+  }
+};
